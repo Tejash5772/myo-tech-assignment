6c9682a663f6a60984eb996c0e5634aabc672573
Tejash Patel <tejaspatel0905@gmail.com>
2026-08-03 17:31:55 +0530

    Initial Angular project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadComponent: () =>
+            import('../layouts/main-layout/main-layout')
+                .then(m => m.MainLayout)
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
